dcdd758606cf0bc7046e867cf9a5dca362a98bc5
bh2980 <bh2980@naver.com>
2023-07-21 02:38:04 +0900

    init: ToDoList Project

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <button className="bg-sky-700 px-4 py-2 text-white hover:bg-sky-800 sm:px-8 sm:py-3">
+        ...
+      </button>
+    </div>
+  );
+}
+
+export default App;

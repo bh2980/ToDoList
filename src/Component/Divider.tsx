9405ded28080331826607f740ca5d1e7fac280da
bh2980 <bh2980@naver.com>
2023-07-26 03:36:00 +0900

    feat: Context API에서 React memo 불가 테스트

--- src/Component/Divider.tsx
+++ src/Component/Divider.tsx
@@ -1,5 +1,7 @@
+import { memo } from "react";
+
 function Divider() {
   return <div className="w-full border border-slate-600"></div>;
 }
 
-export default Divider;
+export default memo(Divider);
--- src/page/Main.tsx
+++ src/page/Main.tsx
@@ -32,6 +32,7 @@ function Main() {
 
   return (
     <div className="text-2xl flex h-screen w-full flex-col items-center justify-center gap-3 bg-slate-900 p-8 text-blue-100">
+      <Divider />
       <ToDoContext.Provider value={{ toDos, dispatchToDos }}>
         <ToDoInput />
         <Divider />
